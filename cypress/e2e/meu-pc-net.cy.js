cb864e27a585e644f0b8cb903ec1085a77b00b06
fermioko <mioko.senac.2023@gmail.com>
2023-08-14 22:16:11 -0300

    validar_login_cypress

--- cypress/e2e/meu-pc-net.cy.js
+++ cypress/e2e/meu-pc-net.cy.js
@@ -1,4 +1,6 @@
+//require('@cypress/xpath');
 describe('Testando Meu Pc.net', () => {
+
   it('Validando cadastro', () => {
     //Entrando no website escolhido meu pc net
     cy.visit('https://meupc.net/')
@@ -35,6 +37,58 @@ describe('Testando Meu Pc.net', () => {
 
     //Verificando se chegamos na tela final do cadastro
     cy.contains('Escolha seu nome de usuário').should('be.visible')
+
+  })
+
+
+  /*
+  LOGIN: testecypress@tuamaeaquelaursa.com
+  SENHA: testecypress
+  VERIFICAR SE A TELA POSSUI O TEXTO “PC atual de TesteCypress”
+  */
+  it.only('Validando login', () => {
+    //Entrando no website escolhido meu pc net
+    cy.visit('https://meupc.net/')
+    
+    //Esperando 1 segundo para executar as funções abaixo
+    cy.wait(1000)
+    //Clicando no notão de menu pela classe .navbar-burger
+    cy.get('.navbar-burger').click()
+    // Clicando no botão de entrar
+    cy
+      .get('ul.menu-list')//Pegando a ul cam o classe .menu-list
+      .children('li')//Pegando os filhos da ul
+      .children('a[href="https://meupc.net/login"]')//Pegando o filho da li que tem i href com o valor https://meupc.net/cadastro
+      .click()
+    
+    //Fazendo Login
+    cy
+      .contains('Email ou nome de usuário')
+      .siblings('input')
+      .type('testecypress@tuamaeaquelaursa.com')
+ 
+    //Colocando Senha (Priiii)
+    //cy.xpath('/html/body/main/section/div/div/form/section/div[4]/span/input').type('testecypress');
+
+    //Colocando Senha
+    cy
+     .contains('Senha')
+     .siblings('span')
+     .children('input')
+     .type('testecypress')
+
+    //Clicando no botão (Priiii)
+    //cy.xpath('/html/body/main/section/div/div/form/section/div[5]/button[2]').click()
+
+    cy
+      .contains('Cancelar')
+      .siblings('button')
+      .click()
+
+    // Parar PC atual de TesteCypress
+
+    cy.contains('PC atual de TesteCypress').should('be.visible')
+    
 
   })
 })
